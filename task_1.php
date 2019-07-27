efa360739294619514ce33a704abacdb84a93ddc
Akop <blrarm25@gmail.com>
2019-07-27 15:20:21 +0300

    not finished task_4

--- task_1.php
+++ task_1.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class First
 {
     function getClassName()
--- task_4/task_4.php
+++ task_4/task_4.php
@@ -1,6 +1,12 @@
 <?php
 include('simplehtmldom_1_9/simple_html_dom.php');
 
-$html = file_get_html('https://www.onliner.by');
-foreach($html->find('a') as $e)
+$html = file_get_html('https://www.onliner.by/');
+foreach($html->find('a.b-main-navigation__link') as $e)
     echo $e->href . '<br>';
+
+
+foreach($html->find('div.b-top-logo') as $e)
+    echo $e->innertext . '<br>';
+
+
